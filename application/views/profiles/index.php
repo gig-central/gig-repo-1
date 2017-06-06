01b39a5ed4669830cee4d9d1dca226faf4ce4976
Diane Selvy <dianeselvy@hotmail.com>
2017-06-06 15:33:22 -0700

    fixes breadcrumb links so they work and are dynamic

--- application/views/profiles/index.php
+++ application/views/profiles/index.php
@@ -3,6 +3,12 @@
 $this->load->view($this->config->item('theme') . 'header');
 
 //php echo '<pre>' . var_dump($profiles) . '</pre>';?>
+
+<ul class="breadcrumb">
+  <li><a href="<?=base_url()?>">Home</a></li>
+  <li class="active">Profiles</li>
+</ul>
+
 <h2><?php echo $title ?></h2>
 <?php foreach ($profiles as $profile): ?>
 <div class="col-sm-12 col-md-6 col-lg-4">
