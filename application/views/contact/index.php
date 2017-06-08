4df09544bff95e38e14bbd2706766ab169f842f1
Gordana Minovska <ekogoca@gmail.com>
2017-06-08 10:52:01 -0700

    Improve styles on the contact page
    The styles on the contact page now match the style of the login page

--- application/views/contact/index.php
+++ application/views/contact/index.php
@@ -18,64 +18,48 @@
 $this->load->view($this->config->item('theme') . 'header');
 ?>
   <div class="container ">
-    <div class="col-lg-8 col-lg-offset-2">
-      
+    <div class="col-lg-4 col-lg-offset-4 well">
       <?php echo validation_errors(); ?>
       <?php $attributes = array('class'=>'form-horizontal');
  echo form_open('contact', $attributes) ?>
- <div class="form-group">
-  <h1 class="text-center">Contact Us</h1>
- </div>
       <div class="form-group">
-        <label for="name" class="col-lg-3 control-label"><em>Name*</label>
-        <div class="col-md-6">
-          <input name="Name" class="form-control" type="text">
-        </div>
-      </div>
-
-      <div class="form-group">
-      <label for="email" class="col-lg-3 control-label">Email*</label>
-
-      <div class="col-md-6">
-        <input name="Email" class="form-control" type="email">
-      </div>
+        <h1 class="text-center">Contact Us</h1>
       </div>
       <div class="form-group">
-      <label for="subject" class="col-lg-3 control-label">Subject*</label>
-
-      <div class="col-md-6">
-
+        <label for="name" class="col-lg-3 control-label hidden">Name</label>
+        <input name="Name" class="form-control" type="text" id="name" placeholder="Name">
+      </div>
+      <div class="form-group">
+        <label for="email" class="col-lg-3 control-label hidden">Email</label>
+        <input name="Email" class="form-control" type="email" placeholder="Email">
+      </div>
+      <div class="form-group">
+        <label for="subject" class="col-lg-3 control-label hidden">Subject</label>
         <select name="Subject" class="form-control inputstl">
-
-        <option value="General Inquiry" selected="selected">General Help & Feedback</option>
-        <option value="Specific Inquiry">Flag Inappropriate Content</option>
-        <option value="Stoopid Inquiry">Employer Support</option>
-        <option value="Stoopid Inquiry">Job Posting Support</option>
-        <option value="Stoopid Inquiry">Press Inquiry</option>
-        <option value="Stoopid Inquiry">Partnership Inquiry</option>
-        <option value="Stoopid Inquiry">Advertising</option>
-    </select>
-      </div>
-      </div>
+          <option value="General Inquiry" selected="selected">General Help & Feedback</option>
+          <option value="Specific Inquiry">Flag Inappropriate Content</option>
+          <option value="Stoopid Inquiry">Employer Support</option>
+          <option value="Stoopid Inquiry">Job Posting Support</option>
+          <option value="Stoopid Inquiry">Press Inquiry</option>
+          <option value="Stoopid Inquiry">Partnership Inquiry</option>
+          <option value="Stoopid Inquiry">Advertising</option>
+        </select>
+      </div>  
+    <div class="form-group">
+      <label for="message" class="col-lg-3 control-label hidden">Message</label>
+      <textarea name="Message" class="form-control" cols="40" rows="5" required placeholder="Your message goes here"></textarea>
+    </div>
+    <div class="form-group">
+      <?php echo $this->recaptcha->render(); ?>
+    </div>
+    
       <div class="form-group">
-      <label for="message" class="col-lg-3 control-label">Message*</label>
-      <div class="col-md-6">
-        <textarea name="Message" class="form-control" cols="40" rows="5" required></textarea>
+        <input name="submit" class="btn btn-default btn-block" type="submit" value="Submit Message" />
       </div>
-      </div>
-      <div class="form-group">  
-        <?php echo $this->recaptcha->render(); ?>
-       </div>
-       <fieldset>
-         <div class="form-actions col-xs-12 col-md-6 col-lg-9">
-        <input name="submit" class="btn btn-default contact-btn" type="submit" value="Submit Message" />
-       </div>
-       </fieldset>
-       
-      </div>
-      </form>
-
-      <?php
+  </form>
+</div>
+</div>
+  <?php
 //$this->load->view('themes/bootswatch/footer'); -old code
 $this->load->view($this->config->item('theme') . 'footer');
 
