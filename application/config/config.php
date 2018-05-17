9f5ccb7fd0d4de6ca254ec16f9bfdfa9e7c50a1e
gillilands19 <gillilands19@gmail.com>
2018-05-16 20:43:02 -0700

    adds todos in gig_model

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'your domain here';
+$config['base_url'] = 'http://localhost:8888/gig-central/';
 
 /*
 |--------------------------------------------------------------------------
