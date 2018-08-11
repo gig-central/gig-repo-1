fcff0bf223b334e17669f8a5c45e4c579617c8f6
Mike Archie <rusty505@yahoo.com>
2018-08-10 19:44:22 -0700

    fix commenting and documentation tags

--- application/views/profiles/success.php
+++ application/views/profiles/success.php
@@ -1,34 +1,20 @@
 <?php
 /**
-* index.php view page for generic profile_form controller
+* view/profile_form/success.php
+* view for profiles/success, which is called inside controller: Profile->add()
 *
-*
-*view/profile_form/index.php
-*
-* @package MediumLARGE_PIECE_OF_PROGRAM
-* @subpackage Profile_form
+* @package gig-central
+* @subpackage application/views/profiles
 * @author Max Diediker <mdiediker@gmail.com>
-* @version 1.0 2015/05/21
-* @link http://www.example.com/
+* @author Mike Archambault <michael.arch@gmail.com
+* @version 1.1 2018/08/10
 * @license http://www.apache.org/licenses/LICENSE-2.0
-* @see Profile_form_model.php
-* @see Profile_form.php
-* @todo none
+* @see Profile.php
+* @todo A nice feature would be to provide a link to the newly created profile.
 */
 
 $this->load->view($this->config->item('theme') . 'header'); //Loads Bootswatch theme and header
 ?>
-
-<!--
-/**
-* Form that accepts data
-*
-*
-* @param none
-* @return void
-* @todo none
-*/
--->
 
 <div class="container">
     <p><?php echo 'You successfully submitted a profile!'; ?><p>
