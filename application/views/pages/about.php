398d8023b7c5a72d79fc090d2b8dd927e02d4cfd
robertohogue <36170635+robertohogue@users.noreply.github.com>
2018-08-15 13:41:58 -0700

    Rewrote the about page to be more appealing
    The original copy was not focused at the user and was just a description. This new one is a little bit more clear that this is an ongoing project and tells the reader what to do next.

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -4,15 +4,19 @@
     <div class="row col-xs-12 col-md-8">
 <h1>About Us</h1>
 <p>
-Gig Central was created in May 2015 as a student project at Seattle Central College.  We are an online meeting place for web developers and employers.  Our goal is to create a community that serves the needs of both companies and technology professionals.</p>
+Gig Central is a robust application helping graduating students and alumni find jobs posted directly by prospective employers. It was designed to put people in direct contact with the right people at hiring organizations. Gig Central is an ongoing development and new features are being added quite frequently. Please come back often to check on your next bold career move and to see how this site grows. </p> 
 <p>
-“Gig” comes from the word “engagement.”  The term gig was coined by Jazz musicians in the 1920s and now refers to a variety of regular and irregular work types in the entertainment field.  A gig can describe almost any type of temporary or incidental employment.</p>
+This site was founded in May 2015 as a student project at Seattle Central College. Each quarter, a new class takes the reigns and puts their own mark on this truly collaborative project. We are creating an online meeting place for web developers and employers. Our goal is to create a community that serves the needs of both companies and technology professionals.</p>
 <p>
-At Gig Central web developers can find work projects that fit their skill sets.  They can advertise and promote their achievements.  Businesses can find individuals with many different types of specialized knowledge areas and experiences.</p>
+“Gig” comes from the word “engagement.” The term gig was coined by Jazz musicians in the 1920s and now refers to a variety of regular and irregular work types in the entertainment field. A gig can describe almost any type of temporary or incidental employment. I this case, it can also mean a full-time position at your favorite tech company.</p>
 <p>
-Most web developers at Gig Central are current students at Seattle Central College.  Our services are also open to recent graduates.  The project is managed by students with advice and support from college instructors.</p>
+At Gig Central, users and web developers can find work projects that fit their skill sets.  They can advertise and promote their achievements. Businesses can find individuals with many different types of specialized knowledge areas and experiences.</p>
 <p>
-Gig Central is based in the Seattle area and is currently limited to local employers.  The service may possibly be extended to other cities later.</p>
+Many web developers on Gig Central are current students at Seattle Central College. Our services are also open to recent graduates as well as any skilled job seeker. Our project is managed and grown by students with advice and support from our knowledgeable college instructors.</p>
+<p>
+Gig Central is based in the Seattle area and is currently limited to local employers. Plans are in the works to extend this service to include other cities and be useful for job seekers in other fields.</p>
+<p> 
+Feel free to click the link in the navigation above to set up your free online profile and dip your toes in the waters of an active and eager job market!</p>        
     </div><!-- end .row -->
 </div><!-- end .container -->
 
