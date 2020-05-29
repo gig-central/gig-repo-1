1f740a383cdef6f2a41a7909a808006f3db08ed3
warangkana watthayo <watthayo.warangkana@seattlecentral.edu>
2020-05-28 17:42:34 -0700

    updates text on welcome page
    updates text in 'find a gig' and 'post a gig' section in welcome page

--- application/views/welcome_page.php
+++ application/views/welcome_page.php
@@ -22,7 +22,7 @@
             <h1><i class="fa fa-search"></i></h1>
             <h3>Find a gig</h3>
             <div class="bar"></div>
-            <p>Are you looking for a gig that you can sharpen your dev skills? Find a gig to work on.</p>
+            <p>Are you looking for a gig that can help you sharpen your dev skills? Find out who has a gig available.</p>
         </div></div>
     </a>
 
@@ -32,7 +32,7 @@
             <h1><i class="fa fa-briefcase"></i></h1>
             <h3>Post a gig</h3>
             <div class="bar"></div>
-            <p>Are you hiring a developer who can help your website building? Share with us</p>
+            <p>Are you looking to hire a developer to help you build your website? Post your gig with us.</p>
         </div></div>
     </a>
 
